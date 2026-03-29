f30b9d9455b8fa7e3eefe0c1add6cb2d7fd26f9c
Varyvonchyk Sviataslau <svatvarivonchik@gmail.com>
2026-03-29 21:28:19 +0300

    price calc and all concepts

---
+++ udemy/priceCalc/prices/prices.go
@@ -0,0 +1,58 @@
+package prices
+
+import (
+	"fmt"
+
+	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/iomanager"
+)
+
+
+type TaxIncludedPriceJob struct{
+	IOManger iomanager.IOManager `json:"-"`
+	TaxRate float64 `json:"tax_rate"`
+	InputPrices []float64 `json:"input_price"`
+	TaxIncludedPrices map[string]string `json:"tax_rate_included_prices"`
+}
+
+func (job *TaxIncludedPriceJob) LoadData() error {
+	
+	lines, err := job.IOManger.ReadLines()
+
+		if err != nil {
+			return err
+		}
+
+	prices,err := conversion.StringToFloats(lines)
+
+		if err != nil {
+			return err
+		}
+
+	job.InputPrices = prices
+	return nil
+}
+
+func (job *TaxIncludedPriceJob) Process () error {
+	err := job.LoadData()
+	if err != nil{
+		return err
+	}
+	result := make(map[string]string)
+	
+		for _, price := range job.InputPrices{
+			TaxIncludedPrice := price * (1+job.TaxRate)
+			result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", TaxIncludedPrice)
+		}
+		job.TaxIncludedPrices = result
+
+		return job.IOManger.WriteResult(job)
+}
+
+func NewTaxIncludedPriceJob(iom iomanager.IOManager,taxRate float64) (TaxIncludedPriceJob){
+	return TaxIncludedPriceJob{
+		IOManger: iom,
+		InputPrices: []float64{10,20,30},
+		TaxRate: taxRate,
+	}
+}
